a6bf43ad49b12ac5c5a0b5252f7c77658d185c28
kuhaha <kevin.yr.cheng@gmail.com>
2023-07-30 09:58:09 +0900

    Separate codes

---
+++ lib/KsuStudent.php
@@ -0,0 +1,58 @@
+<?php
+namespace kst;
+
+include 'KsuCode.php';
+use kst\KsuCode;
+
+class KsuStudent{
+
+    public static function getInfo($sid){
+        // $part: 'FULL', 'DFALT', 'DEPT'
+        $sid = self::validateSid($sid);
+        if (!$sid) return null;//正しい学生番号ではない
+        if (preg_match('/^\d{2}[A-Z]{2}\d{3}$/', $sid)){
+            $stud_yr = substr($sid, 0, 2);
+            $dept_id = substr($sid, 2, 2);
+            $stud_no = substr($sid, 4, 3);
+            if (in_array($dept_id, array_keys(KsuCode::FACULTY_DEPT))){
+                $dept = KsuCode::FACULTY_DEPT[$dept_id];
+            }
+        }
+        if (preg_match('/^\d{2}[A-Z]{3}\d{2}$/', $sid)){
+            $stud_yr = substr($sid, 0, 2);
+            $dept_id = substr($sid, 2, 3);
+            $stud_no = substr($sid, 5, 2);
+            if (in_array($dept_id, array_keys(KsuCode::GRADUATE_SCHL))){
+                $dept = KsuCode::GRADUATE_SCHL[$dept_id];
+            }
+        }
+        $rs = [];
+        if (isset($sid, $stud_yr, $dept_id, $stud_no, $dept)){
+            list ($faculty_name, $dept_name) = explode(' ', $dept);
+            return [$sid, $stud_yr, $dept_id, $stud_no, $faculty_name, $dept_name];
+        } 
+        return null;
+    }
+
+    /** 文字列が学籍番号の標準形に変換可能かをチェックし，可能なら正しい学籍番号を返す
+     * 全角英数を半角に変換、スペースや改行コードを削除、小文字を大文字に変換する */
+    public static function validateSid($str)
+    {
+        $str = preg_replace("/( |　)/", "", trim($str) );//空白文字を削除
+        if (!preg_match("/^[a-zA-Z0-9ａ-ｚA-Z０-９]+$/", $str)){
+            return null;
+        }
+        $str = mb_convert_kana($str, "a");//全角英数を半角英数へ変換
+        $str = strtoupper($str);//小文字を大文字に変換
+   
+        $pattern = implode('|',array_keys(KsuCode::FACULTY_DEPT));//学部IDのパターン
+        if (preg_match('/^\d{2}('. $pattern . ')\d{3}$/', $str)){
+            return $str;
+        }
+        $pattern = implode('|',array_keys(KsuCode::GRADUATE_SCHL));//大学院IDのパターン
+        if (preg_match('/^\d{2}('. $pattern . ')\d{2}$/', $str)){
+            return $str;
+        }
+        return null;
+    }
+}
--- string.php
+++ string.php
@@ -1,150 +1,60 @@
 <?php
+include 'lib/KsuStudent.php';
+use kst\KsuStudent;
 
 header('Content-Type: text/plain; charset=UTF-8');
 
 $str = "１２Ⅾ　D　Ⅾ08";
 echo "'{$str}'\n";
-if ($sid){
-    echo $sid, "\n";
-    echo KsuStudent::getDept($sid), "\n";
-    echo KsuStudent::getDept($sid, 'FALT'), "\n";
-    echo KsuStudent::getDept($sid, 'DEPT'), "\n";
+$student = KsuStudent::getInfo($str);
+if ($student){
+    echo '[', implode(', ', $student), "]\n";
 }else{
-    echo "Unrecognized student id '{$str}'\n";
+    echo "Unrecognized string of student id '{$str}'\n";
 }
 echo "\n";
+// '１２Ⅾ　D　Ⅾ08'
+// Unrecognized string of student id '１２Ⅾ　D　Ⅾ08'
 
-$str = "１２as １　　０ ２　 　";
+$str = "２４as １　　０ ２　 　";
 echo "'{$str}'\n";
-$sid =  KsuStudent::validateSid($str);
-if ($sid){
-    echo $sid, "\n";
-    echo KsuStudent::getDept($sid), "\n";
-    echo KsuStudent::getDept($sid, 'FALT'), "\n";
-    echo KsuStudent::getDept($sid, 'DEPT'), "\n";
+$student = KsuStudent::getInfo($str);
+if ($student){
+    echo '[', implode(', ', $student), "]\n";
+}else{
+    echo "Unrecognized string '{$str}'\n";
 }
 echo "\n";
-
-// OUTPUT: 12RS102
+// '２４as １　　０ ２　 　'
+// [24AS102, 24, AS, 102, 芸術学部, ソーシャルデザイン学科]
 
 $str = "2２GJK   ０　 ３　 　";
 echo "'{$str}'\n";
-$sid =   KsuStudent::validateSid($str);
-if ($sid){
-    echo $sid, "\n";
-    echo KsuStudent::getDept($sid), "\n";
-    echo KsuStudent::getDept($sid, 'FALT'), "\n";
-    echo KsuStudent::getDept($sid, 'DEPT'), "\n";
+$student = KsuStudent::getInfo($str);
+if ($student){
+    echo '[', implode(', ', $student), "]\n";
+}else{
+    echo "Unrecognized string '{$str}'\n";
 }
 echo "\n";
-// OUTPUT: 22GJK03
+// '2２GJK   ０　 ３　 　'
+// [22GJK03, 22, GJK, 03, 情報科学研究科, 情報科学専攻]
 
 $str = "１２DTI　 　　０　 ２　　 ";
 echo "'{$str}'\n";
-$sid = KsuStudent::validateSid($str);
-if ($sid){
-    echo $sid, "\n";
-    echo KsuStudent::getDept($sid), "\n";
-    echo KsuStudent::getDept($sid, 'FALT'), "\n";
-    echo KsuStudent::getDept($sid, 'DEPT'), "\n";
+$student = KsuStudent::getInfo($str);
+if ($student){
+    echo '[', implode(', ', $student), "]\n";
+}else{
+    echo "Unrecognized string '{$str}'\n";
 }
-// OUTPUT: 
+echo "\n";
+// '１２DTI　 　　０　 ２　　 '
+// [12DTI02, 12, DTI, 02, 工学研究科, 産業技術デザイン専攻]
 
 echo "\n\n";
 
-class KsuStudent{
-    const FACULTY_DEPT =[
-        //学科のIDと名称
-        'RS'=>'理工学部 情報科学科',
-        'RM'=>'理工学部 機械工学科',
-        'RE'=>'理工学部 電気工学科',        
-        'LL'=>'生命科学部 生命科学科',        
-        'UA'=>'建築都市工学部 建築学科',
-        'UH'=>'建築都市工学部 住居・インテリア学科',
-        'UC'=>'建築都市工学部 都市デザイン工学科',
-        'CB'=>'商学部 経営・流通学科',
-        'EE'=>'経済学部 経済学科',        
-        'DT'=>'地域共創学部 観光学科',
-        'DR'=>'地域共創学部 地域づくり学科',
-        'AA'=>'芸術学部 芸術表現学科',
-        'AP'=>'芸術学部 写真・映像メディア学科',
-        'AD'=>'芸術学部 ビジュアルデザイン学科',
-        'AE'=>'芸術学部 生活環境デザイン学科',
-        'AS'=>'芸術学部 ソーシャルデザイン学科',        
-        'KK'=>'国際文化学部 国際文化学科',
-        'KN'=>'国際文化学部 日本文化学科',        
-        'HP'=>'人間科学部 臨床心理学科',
-        'HC'=>'人間科学部 子ども教育学科',
-        'HS'=>'人間科学部 スポーツ学科',
-    ];
 
-    //大学院のIDと名称
-    const GRADUATE_SCHL = [   
-        'GBE'=>'経済・ビジネス研究科 経済学専攻',
-        'GBM'=>'経済・ビジネス研究科 現代ビジネス専攻',
-        'GTI'=>'工学研究科 産業技術デザイン',
-        'GJK'=>'情報科学研究科 情報科学専攻',
-        'GAC'=>'芸術研究科 造形表現専攻',
-        'GKK'=>'国際文化研究科 国際文化専攻',
-
-        'DBE'=>'経済・ビジネス研究科 経済学専攻',
-        'DBM'=>'経済・ビジネス研究科 現代ビジネス専攻',
-        'DTI'=>'工学研究科 産業技術デザイン専攻',
-        'DJK'=>'情報科学研究科 情報科学専攻',
-        'DAC'=>'芸術研究科 造形表現専攻',
-        'DKK'=>'国際文化研究科 国際文化専攻',
-    ];
-    /** 標準の学籍番号（半角英数大文字）に変換（全角半角変換、スペースや改行コード削除、大文字変換） */
-    public static function getDept($sid, $part=''){
-        // $part: 'FULL', 'DFALT', 'DEPT'
-        $sid = self::validateSid($sid);
-        if (!$sid) return null;
-        $dept = null; 
-        if (preg_match('/^\d{2}[A-Z]{2}\d{3}$/', $sid)){
-            $dept_id = substr($sid, 2, 2);
-            if (in_array($dept_id, array_keys(self::FACULTY_DEPT))){
-                $dept = self::FACULTY_DEPT[$dept_id];
-            }
-        }
-        if (preg_match('/^\d{2}[A-Z]{3}\d{2}$/', $sid)){
-            $dept_id = substr($sid, 2,3);
-            if (in_array($dept_id, array_keys(self::GRADUATE_SCHL))){
-                $dept = self::GRADUATE_SCHL[$dept_id];
-            }
-        }
-        if (!$dept) return null;
-        list ($flt_name, $dpt_name) = explode(' ', $dept);
-        if ($part == 'DEPT'){
-            return $dpt_name;
-        }
-        if ($part == 'FALT'){
-            return $flt_name;
-        }
-        if ($part == 'FULL'){
-            return $dept;
-        }
-        return $dept_id;
-
-    }
-    
-    public static function validateSid($str)
-    {
-        $str = preg_replace("/( |　)/", "", trim($str) );//空白文字を削除
-        $str = preg_replace("/Ⅾ/", "D", $str );//全角と認識できない文字を変換
-        $str = mb_convert_kana($str, "a");//全角英数を半角英数へ変換
-        $str = strtoupper($str);//小文字を大文字に変換
-    
-        $pattern = implode('|',array_keys(self::FACULTY_DEPT));//学部IDのパターン
-        if (preg_match('/^\d{2}('. $pattern . ')\d{3}$/', $str)){
-            return $str;
-        }
-        $pattern = implode('|',array_keys(self::GRADUATE_SCHL));//大学院IDのパターン
-        if (preg_match('/^\d{2}('. $pattern . ')\d{2}$/', $str)){
-            return $str;
-        }
-        return null;
-    }
-}
 $unit =  \DateInterval::createFromDateString('10 minute');
 echo $unit->format('%I'), "\n";
 // OUTPUT: 10 
